18608d9bba2a0d6c75f808ca7dcec3cd5adeb26a
INDUS2RY <den.prohorov@internet.ru>
2022-10-20 23:24:06 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+import static java.lang.Math.;
+public class Main
+{
+    public static void main(String[] args)
+    {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Введите число: ");
+        int a = in.nextInt();
+        double d = a a;
+        double b = a * a * a;
+        double c = a * a * a * a;
+        System.out.println(d);
+        System.out.println(b);
+        System.out.println(c);
+    }
+}
